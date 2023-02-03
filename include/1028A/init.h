5d6f8e0b027ad95d466db04bcba5da71677686ac
Mustang8192 <107894608+Mustang8192@users.noreply.github.com>
2023-02-03 10:38:51 -0600

    pink Ui

--- include/1028A/init.h
+++ include/1028A/init.h
@@ -31,7 +31,7 @@ namespace _1028A{
     }
 
     namespace ui{
-        void init(int hue = 190, bool repeated = false);
+        void init(int hue = 328, bool repeated = false);
         static lv_fs_res_t pcfs_open( void * file_p, const char * fn, lv_fs_mode_t mode);
         static lv_fs_res_t pcfs_close( void * file_p);
         static lv_fs_res_t pcfs_read( void * file_p, void * buf, uint32_t btr, uint32_t * br);
